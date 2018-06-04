a28ce674650927a9217d7fb4d6c170b41eee5ff4
Peter Spackman <peterspackman@fastmail.com>
2018-06-04 12:48:37 +0100

    updated to working version

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # CrystalExplorer Scripts
 
 A useful collection of scripts to help crystalexplorer usage.
+
+## g09wrapper.py
+
+If you edit this script appropriately for your own cluster i.e. `remote_host`,
+modification of g09 memory and thread usage, changes for slurm rather than torque.
+You should be able to set this as your gaussian executable in crystalexplorer and
+have the jobs run remotely on a cluster.
+
+This assumes you have setup passwordless login (i.e. `ssh-copy-id` etc.) on the
+remote host.
+
+Further, since this script checks the job status every so often (30s by default),
+it is a good idea to setup `ServerAliveInterval 300` or something similar so
+that you do not startup a new ssh connection for each command.
+
+Also be aware this script is a work in progress, and was hastily put together so
+use at your own risk.
--- g09wrapper.py
+++ g09wrapper.py
@@ -6,35 +6,49 @@ import argparse
 import time
 
 LOG = logging.getLogger('g09wrapper')
-LOG.setLevel(logging.INFO)
+LOG.setLevel(logging.DEBUG)
 
 class RemoteJob(object):
     commands = {
-        'submit': 'qsub -q test -N {job_name}',
+        'submit': 'qsub -N {job_name} {job_file}',
         'check_status': "qstat -f {job_id} | awk '/job_state/ {{print $NF}}' ",
     }
 
     job_script = """#!/bin/bash
-PBS -S /bin/bash
-PBS -l walltime=00:10:00
-PBS -l nodes=1:ppn=16
+#PBS -S /bin/bash
+#PBS -l walltime=01:00:00
+#PBS -l mem=32GB
+#PBS -l nodes=1:ppn=16
 
 # module load gaussian
 
-cd $PBS_O_WORKDIR
+export GAUSS_MDEF=30GB
+export GAUSS_CDEF=0-15
+
+cd {remote_wd}
 pwd
 {g09_command} {filename}
+exit $?
 """
 
     config = {
-        'remote_host': 'localhost',
+        'remote_host': '',
         'remote_test_command': 'hostname',
-        'remote_wd_setup': 'mkdir -p /tmp/$USER/{job_name}',
-        'remote_job_submit': 'qsub -N {job_name}',
-        'check_status_period': 1,
+        'remote_wd': '/scratch/$USER/{job_name}',
+        'remote_wd_setup': 'mkdir -p {remote_wd}',
+        'remote_job_submit': 'cd {remote_wd} && {submit_command}',
+        'check_status_period': 30, # seconds between checking in
         'waiting_states': {'R', 'Q'},
         'complete_states': {'C', 'E'}
     }
+
+    job_id = None
+    job_name = "Unknown"
+    job_status = None
+    job_file = None
+    job_inputs = []
+    job_outputs = []
+    working_directory = None
 
     def __init__(self, input_filename):
 
@@ -43,16 +57,26 @@ pwd
                       'variables such as remote host etc.')
             sys.exit(1)
 
-        self.job_id = None
         self.connection_valid = self.check_connection()
-        self.job_status = None
-        self.jobfile_contents = self.job_script.format(filename=input_filename,
-                                                 g09_command='cat')
         self.job_name = input_filename[:-4] # remove suffix
-        LOG.info('')
-        with open(self.job_name+'.jobfile', 'w') as f:
-            f.write(self.jobfile_contents)
+        self.job_file = self.job_name+'.jobfile'
+        self.working_directory = self.config['remote_wd'].format(job_name=self.job_name)
+        self.job_inputs = [input_filename, self.job_file]
+        self.setup_job_outputs()
 
+        self.jobfile_contents = self.job_script.format(
+                filename=input_filename,
+                g09_command='g09',
+                remote_wd=self.working_directory)
+        with open(self.job_file, 'w') as f:
+            f.write(self.jobfile_contents)
+        self.submit_job()
+
+    def setup_job_outputs(self):
+        fmt_string = '{remote_host}:{remote_wd}/{{f}}'.format(
+                remote_host=self.config['remote_host'],
+                remote_wd=self.working_directory)
+        self.job_outputs = [fmt_string.format(f=f) for f in (self.job_name+'.log', 'Test.FChk')]
 
     def connect_and_execute(self, command):
         cmd = ["ssh", self.config['remote_host'], command]
@@ -80,11 +104,8 @@ pwd
 
 
     def make_working_directory(self):
-        cmd = self.commands['remote_wd_setup'].format(job_name=self.job_name)
+        cmd = self.config['remote_wd_setup'].format(remote_wd=self.working_directory)
         ssh = self.connect_and_execute(cmd)
-        result = ssh.stdout.read().strip()
-        if not self.job_status:
-            self.job_status = 'Unknown'
 
     def check_connection(self):
         LOG.debug("Testing connection to remote host '%s'", self.config['remote_host'])
@@ -103,20 +124,75 @@ pwd
         return True
 
 
+    def upload_files(self):
+        cmd = ["scp"] + self.job_inputs + \
+                [self.config['remote_host'] + ':' + self.working_directory]
+
+        scp = subprocess.Popen(cmd, shell=False,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        scp.wait()
+        if not scp.returncode == 0:
+            LOG.error("Copying files '%s' to remote host exit status = %d",
+                      self.job_inputs,
+                      scp.returncode)
+            LOG.error("stderr:\n%s", ssh.stderr.read())
+            sys.exit(ssh.returncode)
+
+    def download_files(self):
+        LOG.info('Downloading g09 log file')
+        cmd = ["scp", self.job_outputs[0], './remote.log']
+        scp = subprocess.Popen(cmd, shell=False,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        scp.wait()
+        if not scp.returncode == 0:
+            LOG.error("Copying files '%s' from remote host exit status = %d",
+                      self.job_outputs,
+                      scp.returncode)
+            LOG.error("stderr:\n%s", scp.stderr.read())
+            sys.exit(scp.returncode)
+
+        with open('remote.log') as f:
+            contents = f.read()
+
+        LOG.info('Log contents:\n%s', contents)
+
+        LOG.info('Downloading Test.FChk')
+        cmd = ["scp", self.job_outputs[1],  './']
+        scp = subprocess.Popen(cmd, shell=False,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        scp.wait()
+        if not scp.returncode == 0:
+            LOG.error("Copying files '%s' from remote host exit status = %d",
+                      self.job_outputs,
+                      scp.returncode)
+            LOG.error("stderr:\n%s", scp.stderr.read())
+            sys.exit(scp.returncode)
+
+
     def submit_job(self):
-        LOG.info("Connecting to remote host '%s'", self.config['remote_host'])
-        ssh = self.connect_and_execute(self.config['remote_test_command'])
-        LOG.info("Done")
+
+        self.make_working_directory()
+        self.upload_files()
+
+        LOG.info('Submitting remote job from %s', self.job_file)
+        submit_command = self.commands['submit'].format(job_name=self.job_name,
+                                                        job_file=self.job_file)
+        LOG.info('Submit command = `%s`', submit_command)
+        job_submit = self.config['remote_job_submit'].format(remote_wd=self.working_directory,
+                                                             submit_command=submit_command)
+        ssh = self.connect_and_execute(job_submit)
         self.job_id = ssh.stdout.read().strip()
+        LOG.info('Submitted job ID: %s', self.job_id)
         if not self.job_id:
             error_output = '\n'.join(ssh.stderr.readlines())
             LOG.error('Error submitting job (job_id = %s): %s', self.job_id, error_output)
+            sys.exit(1)
 
 
 
 def setup_logging(args):
-    handler = logging.FileHandler('test.log')
-    formatter = logging.Formatter('[%(name)-4s %(levelname)-3s]: %(msg)s')
+    handler = logging.FileHandler(args.filename[:-4]+'.log')
+    formatter = logging.Formatter('[%(name)-4s %(levelname)-3s %(asctime)s]: %(message)s')
     handler.setFormatter(formatter)
     LOG.addHandler(handler)
 
@@ -131,3 +207,6 @@ if __name__ == '__main__':
     while job.running():
         job.check_status()
         LOG.info('Status for job_id=%s: %s', job.job_id, job.job_status)
+    job.download_files()
+    LOG.infor('Gaussian job %s complete', args.filename)
+    sys.exit(0)
